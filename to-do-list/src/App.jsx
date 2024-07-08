0d7f4f69d4fab86e8fa643e8ac52c4e1ec2dff8e
Kauã Lopes <lopeskazin@gmail.com>
2024-07-08 16:00:22 -0300

    08/07
    .

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,69 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import MinhaImagem from './images/agendamento.png'
-import ImagemGithub from './images/icons8-github-30.png'
-import ImagemLinkedin from './images/Linked_img.png'
-import ImagemPortifólio from './images/Portifólio_img.png'
-import AbrirPopUps from './functions_frontend/AbrirPopUps';
+import To_Do_List from './Pages/To_Do_List'
+
+
 
 function App() {
   return (
-    <div className='container'>
-      <div className='cabecalhostyle'>
-        <h1>TO-DO-LIST</h1>
-        <img className='Imgstyle' src={MinhaImagem} alt="" />
-      </div>
-      <div>
-        <div className='containerinputlogin'>
-          <div className='containerfaçalogin'>
-            <h3>Faça Login</h3>
-          </div>
-          <div className='styleborderinput'>
-            <form>
-              <div className='containerinputmargin '>
-                <label className='stylelabel' htmlFor="nome">Username:</label>
-                <input
-                  placeholder='insira seu login*'
-                  className='styleinputs'
-                  type='text'
-                  name='nome'
-                ></input>
-              </div>
-              <div className='containerinputmargin ' >
-                <label className='stylelabel' htmlFor="email">Email:</label>
-                <input
-                  type="text"
-                  name='email'
-                  placeholder='Insira o seu email *'
-                  className='styleinputs' />
-              </div>
-              <div className='containerinputmargin '>
-                <label className='stylelabel' htmlFor="senha">Senha:</label>
-                <input
-                  placeholder='Insira sua senha*'
-                  type='password'
-                  name='senha'
-                  className='styleinputs' />
-              </div>
-            </form>
-            <div className='containerbuttonentrar'>
-              <button className='buttonentrarstyle' >Entrar</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className='containerbuttonsstyle' >
-        <button id="popupid" className="buttonstyle" onClick={AbrirPopUps} type="button"><img className='popupsstyle' src={ImagemPortifólio} alt="" /></button>
+    <>
+      <To_Do_List></To_Do_List>
 
-        <button className="buttonstyle" onClick="abrirpop_linkd()" type="button"><img className='popupsstyle' src={ImagemLinkedin} alt="" /></button>
-
-        <button className="buttonstyle" onclick="abrirpop_github()" type="button"><img className='popupsstyle' src={ImagemGithub} alt="" /></button>
-      </div>
-      <div className='containerparagraforedes'>
-        <p>Desenvolvido por Kauã Lopes Monteiro</p>
-      </div>
-    </div>
-  );
+    </> 
+  )
 }
-
 export default App;
---
+++ to-do-list/src/Pages/To_Do_List.jsx
@@ -0,0 +1,70 @@
+import ImagemCalendario from './imagens/agendamento.png'
+import ImagemGithub from './images/icons8-github-30.png'
+import ImagemLinkedin from './images/Linked_img.png'
+import ImagemPortifólio from './images/Portifólio_img.png'
+import AbrirPopUps from './functions_frontend/AbrirPopUps';
+import './To_do_List.css'
+
+
+function To_Do_List () {
+
+      return (
+        <div className='container'>
+          <div className='cabecalhostyle'>
+            <h1>TO-DO-LIST</h1>
+            <img className='Imgstyle' src={ImagemCalendario} alt="" />
+          </div>
+          <div>
+            <div className='containerinputlogin'>
+              <div className='containerfaçalogin'>
+                <h3>Faça Login</h3>
+              </div>
+              <div className='styleborderinput'>
+                <form>
+                  <div className='containerinputmargin '>
+                    <label className='stylelabel' htmlFor="nome">Username:</label>
+                    <input
+                      placeholder='insira seu login*'
+                      className='styleinputs'
+                      type='text'
+                      name='nome'
+                    ></input>
+                  </div>
+                  <div className='containerinputmargin ' >
+                    <label className='stylelabel' htmlFor="email">Email:</label>
+                    <input
+                      type="text"
+                      name='email'
+                      placeholder='Insira o seu email *'
+                      className='styleinputs' />
+                  </div>
+                  <div className='containerinputmargin '>
+                    <label className='stylelabel' htmlFor="senha">Senha:</label>
+                    <input
+                      placeholder='Insira sua senha*'
+                      type='password'
+                      name='senha'
+                      className='styleinputs' />
+                  </div>
+                </form>
+                <div className='containerbuttonentrar'>
+                  <button className='buttonentrarstyle' >Entrar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='containerbuttonsstyle' >
+            <button id="popupid" className="buttonstyle" onClick={AbrirPopUps} type="button"><img className='popupsstyle' src={ImagemPortifólio} alt="" /></button>
+    
+            <button className="buttonstyle" onClick="abrirpop_linkd()" type="button"><img className='popupsstyle' src={ImagemLinkedin} alt="" /></button>
+    
+            <button className="buttonstyle" onclick="abrirpop_github()" type="button"><img className='popupsstyle' src={ImagemGithub} alt="" /></button>
+          </div>
+          <div className='containerparagraforedes'>
+            <p>Desenvolvido por Kauã Lopes Monteiro</p>
+          </div>
+        </div>
+      );
+    }
+
+export default To_Do_List
