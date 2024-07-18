819710fae9055b5e47508d8a4bfde5f7bb8b7c91
Kazinh_o <lopeskazin@gmail.com>
2024-07-17 21:52:16 -0300

    17/07

--- to-do-list/src/rotes/Settings.jsx
+++ to-do-list/src/rotes/Settings.jsx
@@ -6,6 +6,7 @@ import './Criarlembretes.css'
 import ImagemCalendario from './/imagens/agendamento.png'
 import Imagemseta from './imagens/seta-para-baixo.png'
 import { useState} from 'react'
+// import ImagemProfile from ''
 
 
 function Settings () {
@@ -72,7 +73,57 @@ function Settings () {
                 </div>
 
             <div className="container_settings" >
+                <div className='container_fotoperfil' >
+                    <div className='style_fotoperfil' >
+                        <div className='img_fotoperfil'>
+                            <img className='style_imgprofile' src="" />
+                        </div>
+                        <div className='container_input_arquivofoto' >
+                            <input type="file" />
+                        </div>
+                        <div>
+                            <button className='button_salvarconfigs' type='submit'>Salvar</button>
+                        </div>
+                    </div>
+                </div>
 
+                <div className='container_userinf'>
+                    <div className='container_tittle' >
+                            <h2 className='style_tittle' >Configurações</h2>
+                    </div>
+                    <div className='container_inputs'>
+                        <div>
+                            <input 
+                            className='style_inputs'
+                            type="text"
+                            name='name'
+                            />
+                        </div>
+                        <div>
+                            <input 
+                            className='style_inputs' 
+                            type="text"
+                            name='sobrenome'
+                             />
+                        </div>
+                        <div >
+                            <input 
+                            className='style_inputs' 
+                            type="text"
+                            name='email'
+                             />
+                        </div>
+                        <div >
+                            <input 
+                            className='style_inputs'
+                            type="text"
+                             />
+                        </div>
+
+                    </div>
+
+                </div>
+               
             </div>
         </div>
     )
