c2c8ab50c86d3633c46fd6bfc5f1c60064815a90
Kauã Lopes <lopeskazin@gmail.com>
2024-08-21 16:01:52 -0300

    21/08
    alterações no css

--- to-do-list/src/rotes/Login.jsx
+++ to-do-list/src/rotes/Login.jsx
@@ -10,6 +10,7 @@ import InputUsernameLogin from '../functionsfrontend/InputUsernameLogin'
 import { useNavigate } from "react-router-dom";
 
 
+
 function To_Do_List() {
 
   const navigate = useNavigate('')
@@ -54,64 +55,76 @@ function To_Do_List() {
 
 
   return (
-    <div className='container'>
-      <div className='cabecalhostyle'>
-        <h1>TO-DO-LIST</h1>
-        <img className='Imgstyle_criarconta' src={ImagemCalendario} alt="" />
-      </div>
-      <div>
-        <div className='containerinputlogin'>
-          <div className='containerfaçalogin'>
-            <h3>Faça Login</h3>
-          </div>
-          <div className='styleborderinput'>
-            <form>
-              <InputUsernameLogin setUsername={setUsername} />
-              <div className='containerinputmargin ' >
-                <input
-                  type="text"
-                  name='email'
-                  placeholder='Insira seu email*'
-                  className='styleinputs'
-                />
-              </div>
-              <div className='containerinputmargin '>
-                <input
-                  placeholder='Insira sua senha*'
-                  type='password'
-                  name='senha'
-                  className='styleinputs'
-                  onChange={(e) => setSenha(e.target.value)} />
-              </div>
-              <div className='containerbuttonentrar11'>
-                <button onClick={handleenteraccount} className='buttonentrarstyle' >Entrar</button>
-              </div>
 
-            </form>
+    <html lang="pt-br">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+          <div className='container'>
+          <div className='cabecalhostyle'>
+            <h1>TO-DO-LIST</h1>
+            <img className='Imgstyle_criarconta' src={ImagemCalendario} alt="" />
           </div>
-          <div className='container_esquecisenha_criarconta' >
-            <div>
-              <a onClick={navigateesquecisenha} className='style_esquecisenha'>esqueceu sua senha?</a>
-            </div>
-            <div>
-              <a onClick={handlenavigate} className='style_criarconta'>criar conta</a>
+          <div>
+            <div className='containerinputlogin'>
+              <div className='containerfaçalogin'>
+                <h3>Faça Login</h3>
+              </div>
+              <div className='styleborderinput'>
+                <form>
+                  <InputUsernameLogin setUsername={setUsername} />
+                  <div className='containerinputmargin ' >
+                    <input
+                      type="text"
+                      name='email'
+                      placeholder='Insira seu email*'
+                      className='styleinputs'
+                    />
+                  </div>
+                  <div className='containerinputmargin '>
+                    <input
+                      placeholder='Insira sua senha*'
+                      type='password'
+                      name='senha'
+                      className='styleinputs'
+                      onChange={(e) => setSenha(e.target.value)} />
+                  </div>
+                  <div className='containerbuttonentrar11'>
+                    <button onClick={handleenteraccount} className='buttonentrarstyle' >Entrar</button>
+                  </div>
+
+                </form>
+              </div>
+              <div className='container_esquecisenha_criarconta' >
+                <div>
+                  <a onClick={navigateesquecisenha} className='style_esquecisenha'>esqueceu sua senha?</a>
+                </div>
+                <div>
+                  <a onClick={handlenavigate} className='style_criarconta'>criar conta</a>
+                </div>
+              </div>
             </div>
           </div>
+          <div className='containerbuttonsstyle' >
+            {<button id="popupid" className="buttonstyle" onClick={() => { window.open("https://kaualopesmonteiro.netlify.app/") }} type="button"><img className='popupsstyle' src={ImagemPortifólio} alt="" /></button>}
+
+            {<button className="buttonstyle" onClick={() => { window.open("https://www.linkedin.com/in/kau%C3%A3-lopes-monteiro/") }} type="button"><img className='popupsstyle' src={ImagemLinkedin} alt="" /></button>}
+
+            {<button className="buttonstyle" onClick={() => { window.open("https://github.com/Kazinhoo0") }} type="button" ><img className='popupsstyle' src={ImagemGithub} alt="" /></button>}
+
+          </div>
+          <div>
+            <p className='containerparagraforedes'>Desenvolvido por Kauã Lopes Monteiro</p>
+          </div>
+
         </div>
-      </div>
-      <div className='containerbuttonsstyle' >
-        {<button id="popupid" className="buttonstyle" onClick={() => { window.open("https://kaualopesmonteiro.netlify.app/") }} type="button"><img className='popupsstyle' src={ImagemPortifólio} alt="" /></button>}
-
-        {<button className="buttonstyle" onClick={() => { window.open("https://www.linkedin.com/in/kau%C3%A3-lopes-monteiro/") }} type="button"><img className='popupsstyle' src={ImagemLinkedin} alt="" /></button>}
-
-        {<button className="buttonstyle" onClick={() => { window.open("https://github.com/Kazinhoo0") }} type="button" ><img className='popupsstyle' src={ImagemGithub} alt="" /></button>}
-
-      </div>
-      <div>
-        <p className='containerparagraforedes'>Desenvolvido por Kauã Lopes Monteiro</p>
-      </div>
-
-    </div>
+      </body>
+        
+      
+    </html>
   );
 }
 
