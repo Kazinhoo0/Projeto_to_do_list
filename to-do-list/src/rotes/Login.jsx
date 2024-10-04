3a20e38bec486697655f01d08686250ad3fdacda
Kauã Lopes <lopeskazin@gmail.com>
2024-10-04 11:19:12 -0300

    04/10 .2
    .

--- to-do-list/src/rotes/Login.jsx
+++ to-do-list/src/rotes/Login.jsx
@@ -36,7 +36,7 @@ function To_Do_List() {
   const handleenteraccount = async (event) => {
     event.preventDefault();
 
-    const response = await fetch('https://projeto-to-do-list-2.onrender.com//login', {
+    const response = await fetch('https://projeto-to-do-list-2.onrender.com/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, senha })
